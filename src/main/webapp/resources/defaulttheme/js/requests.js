cc50dd71b4f59b66ce6b6b9aa37bd96cce53472b
LeonAdeoye <leon.adeoye@gmail.com>
2015-05-22 22:28:55 +0800

    improved javascript for enable and disabling of requests view controls

--- src/main/webapp/resources/defaulttheme/js/requests.js
+++ src/main/webapp/resources/defaulttheme/js/requests.js
@@ -1,17 +1,58 @@
+function enableAddButton()
+{
+	$("input#requests_add_button").removeAttr('disabled');
+}
+
+function disableAddButton()
+{
+	$("input#requests_add_button").attr('disabled', 'disabled');
+}
+
+function snippetMatches(snippet)
+{
+	return SNIPPET_REGEX.test(snippet);
+}
+
+function toggleAddButtonState()
+{
+	if(snippetMatches($("input#requests_snippet")))
+		enableAddButton();
+	else
+		disableAddButton();
+}
+
+function myTrim(x)
+{
+	return x.replace(/^\s+|\s+$/gm,'');
+}
+
+$(document).ready(disableAddButton);
+
 $(".new_request").click(function()
 {
-	if($(this).attr("value") == $(this).attr("default_value"))
-		$(this).attr("value") = "";
+	if($(this).val() == $(this).attr("default_value"))
+	{
+		$(this).value("");
+		
+		if($(this).is("input#requests_snippet")
+			disableAddButton();
+	}
 });
 
 $(".new_request").focusout(function()
 {
-	if($(this).attr("value") == "")
-		$(this).attr("value") = $(this).attr("default_value");
+	if(myTrim($(this).val())) == "")
+		$(this).val($(this).attr("default_value"));
 });
 
 $("#requests_clear_button").click(function()
 {
-	$("#requests_snippet").attr("value", "");
-	$("#requests_bookCode").attr("value", "");
+	$("input.new_request").val($(this).attr("default_value"));
+	disableAddButton();
 });
+
+var SNIPPET_REGEX = /^([+-]?[1-9]*[CP]{1}){1}([-+]{1}[1-9]*[CP]{1})* ([\d]+){1}(,{1}[\d]+)* "
+	+ "[\d]{1,2}(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)20[\d]{2}(,{1}[\d]{1,2}(Jan|Feb|Mar"
+	+ "|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)20[\d]{2})* (\w){4,7}\.[A-Z]{1,2}(,{1}(\w){4,7}\.[A-Z]{1,2})*$/;
+
+
