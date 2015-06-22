8fa87cb73908220b393027f2c09b7125bfb39fb1
LeonAdeoye <leon.adeoye@gmail.com>
2015-06-22 23:35:02 +0800

    added price and dividend members to Underlying detail implementation

--- src/main/java/com/leon/rfq/domains/UnderlyingDetailImpl.java
+++ src/main/java/com/leon/rfq/domains/UnderlyingDetailImpl.java
@@ -1,5 +1,7 @@
 package com.leon.rfq.domains;
 
+import java.math.BigDecimal;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -21,6 +23,11 @@ public class UnderlyingDetailImpl
 	@NotNull(message="{underlying.validation.description.notNull}")
 	@Size(min=1, max=45, message="{underlying.validation.description.size}")
 	private String description;
+	
+	private BigDecimal spread;
+	private BigDecimal priceMean;
+	private BigDecimal priceVariance;
+	private BigDecimal dividendYield;
 	
 	private boolean isValid;
 	
@@ -86,44 +93,182 @@ public class UnderlyingDetailImpl
 		this.isValid = isValid;
 	}
 
-	@Override
-	public String toString()
+	public BigDecimal getSpread()
 	{
-		StringBuilder buf = new StringBuilder("RIC: ");
-		buf.append(this.ric);
-		buf.append(", Description: ");
-		buf.append(this.description);
-		buf.append(", Is valid: ");
-		buf.append(this.isValid);
-		buf.append(", LastUpdatedBy: ");
-		buf.append(this.lastUpdatedBy);
-		
-		return buf.toString();
+		return this.spread;
+	}
+
+	public void setSpread(BigDecimal spread)
+	{
+		this.spread = spread;
+	}
+
+	public BigDecimal getPriceMean()
+	{
+		return this.priceMean;
+	}
+
+	public void setPriceMean(BigDecimal priceMean)
+	{
+		this.priceMean = priceMean;
+	}
+
+	public BigDecimal getPriceVariance()
+	{
+		return this.priceVariance;
+	}
+
+	public void setPriceVariance(BigDecimal priceVariance)
+	{
+		this.priceVariance = priceVariance;
+	}
+
+	public BigDecimal getDividendYield()
+	{
+		return this.dividendYield;
+	}
+
+	public void setDividendYield(BigDecimal dividendYield)
+	{
+		this.dividendYield = dividendYield;
 	}
 
 	@Override
-	public boolean equals(Object o)
+	public String toString()
 	{
-		if(this == o)
-			return true;
-
-		if(!(o instanceof UnderlyingDetailImpl))
-			return false;
-
-		UnderlyingDetailImpl underlying = (UnderlyingDetailImpl) o;
-
-		return this.ric.equals(underlying.ric) && this.description.equals(underlying.description)
-				&& this.lastUpdatedBy.equals(underlying.lastUpdatedBy) && (this.isValid == underlying.isValid);
+		StringBuilder builder = new StringBuilder();
+		builder.append("UnderlyingDetailImpl [ric=");
+		builder.append(this.ric);
+		builder.append(", description=");
+		builder.append(this.description);
+		builder.append(", spread=");
+		builder.append(this.spread);
+		builder.append(", priceMean=");
+		builder.append(this.priceMean);
+		builder.append(", priceVariance=");
+		builder.append(this.priceVariance);
+		builder.append(", dividendYield=");
+		builder.append(this.dividendYield);
+		builder.append(", isValid=");
+		builder.append(this.isValid);
+		builder.append(", lastUpdatedBy=");
+		builder.append(this.lastUpdatedBy);
+		builder.append("]");
+		return builder.toString();
 	}
 
 	@Override
 	public int hashCode()
 	{
-		int result = 17;
-		result = (37 * result) + (this.ric == null ? 0 : this.ric.hashCode());
-		result = (37 * result) + (this.description == null ? 0 : this.description.hashCode());
-		result = (37 * result) + (this.lastUpdatedBy == null ? 0 : this.lastUpdatedBy.hashCode());
-		result = (37 * result) + (this.isValid ? 0 : 1);
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result)
+				+ ((this.description == null) ? 0 : this.description.hashCode());
+		result = (prime * result)
+				+ ((this.dividendYield == null) ? 0 : this.dividendYield.hashCode());
+		result = (prime * result) + (this.isValid ? 1231 : 1237);
+		result = (prime * result)
+				+ ((this.lastUpdatedBy == null) ? 0 : this.lastUpdatedBy.hashCode());
+		result = (prime * result)
+				+ ((this.priceMean == null) ? 0 : this.priceMean.hashCode());
+		result = (prime * result)
+				+ ((this.priceVariance == null) ? 0 : this.priceVariance.hashCode());
+		result = (prime * result) + ((this.ric == null) ? 0 : this.ric.hashCode());
+		result = (prime * result) + ((this.spread == null) ? 0 : this.spread.hashCode());
 		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (!(obj instanceof UnderlyingDetailImpl))
+		{
+			return false;
+		}
+		UnderlyingDetailImpl other = (UnderlyingDetailImpl) obj;
+		if (this.description == null)
+		{
+			if (other.description != null)
+			{
+				return false;
+			}
+		} else if (!this.description.equals(other.description))
+		{
+			return false;
+		}
+		if (this.dividendYield == null)
+		{
+			if (other.dividendYield != null)
+			{
+				return false;
+			}
+		} else if (!this.dividendYield.equals(other.dividendYield))
+		{
+			return false;
+		}
+		if (this.isValid != other.isValid)
+		{
+			return false;
+		}
+		if (this.lastUpdatedBy == null)
+		{
+			if (other.lastUpdatedBy != null)
+			{
+				return false;
+			}
+		} else if (!this.lastUpdatedBy.equals(other.lastUpdatedBy))
+		{
+			return false;
+		}
+		if (this.priceMean == null)
+		{
+			if (other.priceMean != null)
+			{
+				return false;
+			}
+		} else if (!this.priceMean.equals(other.priceMean))
+		{
+			return false;
+		}
+		if (this.priceVariance == null)
+		{
+			if (other.priceVariance != null)
+			{
+				return false;
+			}
+		} else if (!this.priceVariance.equals(other.priceVariance))
+		{
+			return false;
+		}
+		if (this.ric == null)
+		{
+			if (other.ric != null)
+			{
+				return false;
+			}
+		} else if (!this.ric.equals(other.ric))
+		{
+			return false;
+		}
+		if (this.spread == null)
+		{
+			if (other.spread != null)
+			{
+				return false;
+			}
+		} else if (!this.spread.equals(other.spread))
+		{
+			return false;
+		}
+		return true;
+	}
+	
 }
